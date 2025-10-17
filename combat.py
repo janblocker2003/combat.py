a4500052cf2ed12dbe6e646cc08c999175445d95
janblocker2003 <janblocker2003@gmail.com>
2025-10-17 15:46:34 -0400

    Final version

--- combat.py
+++ combat.py
@@ -3,20 +3,20 @@
 #10/13/2025
 #My Very Own combat.py
 
-import tbc.py
+import tbc
 
 def main():
-    hero = Character()
+    hero = tbc.Character()
     hero.name = "Hero"
     hero.hitPoints = 10
     hero.hitChance = 50
     hero.maxDamage = 5
     hero.armor = 2
-    monster = Character("Monster", 20, 30, 5, 0)
+    monster = tbc.Character("Monster", 20, 30, 5, 0)
     hero.printStats()
     monster.printStats()
     
-    Character.fight(monster, hero)
+    tbc.Character.fight(monster, hero)
     
 if __name__ == "__main__":
     main()
